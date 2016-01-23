5b141176a95384b4a6f7d881f5bdbd6e5f8b16d4
Isaac Stefanek <Isaac.Stefanek@corbis.com>
2016-01-22 16:51:01 -0800

    Fixed listen host

--- tests/server.js
+++ tests/server.js
@@ -3,6 +3,6 @@ var http = require('http');
 http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
     response.end('Hello World!\n');
-}).listen(3000);
+}).listen(3000, "0.0.0.0");
 
 console.log('Server started');
